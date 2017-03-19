402057602c1ac7189b078c0076933b88067523ff
Sujata Dev <sujatadev97@gmail.com>
2017-03-20 01:06:30 +0530

    for displaying the shortest path

---
+++ src/algorithms/shortest_path.c
@@ -0,0 +1,57 @@
+//For displaying the shortest path
+
+#include "../defs/functions.h"
+
+void shortest_path(char str[])
+{
+    int i=strlen(str)-1,j=0,k=0;
+    char ch;
+    while(i>j)
+    {
+        ch=str[i];
+        str[i]=str[j];
+        str[j]=ch;
+        i--;
+        j++;
+    }
+    printf("\nShortest Path:\n\n");
+    printf("\tStart-> ");
+    while(str[k]!='\0')
+    {
+        switch(str[k])
+        {
+            case 'A':   printf("Hostel-A-> ");
+                        break;
+            case 'B':   printf("Hostel-B-> ");
+                        break;
+            case 'C':   printf("Hostel-C-> ");
+                        break;
+            case 'D':   printf("Hostel-D-> ");
+                        break;
+            case 'E':   printf("Hostel-E-> ");
+                        break;
+            case 'F':   printf("Hostel-F-> ");
+                        break;
+            case 'G':   printf("Hostel-G-> ");
+                        break;
+            case 'H':   printf("Hostel-H-> ");
+                        break;
+            case 'I':   printf("Hostel-S-> ");
+                        break;
+            case 'J':   printf("CCD-> ");
+                        break;
+            case 'K':   printf("Symbus-> ");
+                        break;
+            case 'L':   printf("Mess-> ");
+                        break;
+            case 'M':   printf("Rangoli-> ");
+                        break;
+            case 'N':   printf("Coffee-Stop-> ");
+                        break;
+            case 'O':   printf("Main-Gate-> ");
+                        break;
+        }
+        k++;
+    }
+    printf("End\n");
+}
