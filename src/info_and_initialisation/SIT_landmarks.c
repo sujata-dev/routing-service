8f941fbf6b141ef268f1dc8d6674f1484d6d32e4
Sujata Dev <sujatadev97@gmail.com>
2017-03-20 01:19:29 +0530

    list of landmarks of SIT

---
+++ src/info_and_initialisation/SIT_landmarks.c
@@ -0,0 +1,23 @@
+//List of landmarks of SIT
+
+#include "../defs/functions.h"
+
+void SIT_landmarks()
+{
+    printf("\n\nList of major landmarks of SIT to visit:\n");
+    printf("1) Hostel-A\n");
+    printf("2) Hostel-B\n");
+    printf("3) Hostel-C\n");
+    printf("4) Hostel-D\n");
+    printf("5) Hostel-E\n");
+    printf("6) Hostel-F\n");
+    printf("7) Hostel-G\n");
+    printf("8) Hostel-H\n");
+    printf("9) Hostel-S\n");
+    printf("10) CCD (Cafe Coffee Day)\n");
+    printf("11) Symbus\n");
+    printf("12) Mess\n");
+    printf("13) Rangoli\n");
+    printf("14) Coffee-Stop\n");
+    printf("15) Main-Gate\n");
+}
