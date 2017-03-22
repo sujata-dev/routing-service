b2b174a02c90c2b154f92d4f706974c6102792df
Sujata Dev <sujatadev97@gmail.com>
2017-03-23 02:42:12 +0530

    to read the stored information from file

---
+++ src/function_handling.c
@@ -0,0 +1,35 @@
+//To read the stored information from files
+
+#include "defs/functions.h"
+
+void function_handling(int x)
+{
+    FILE *fp;
+    char c,d,y=x+32;
+    fp=fopen("/home/administrator/Desktop/routing-service/src/file.dat","r");
+    if(fp==NULL)
+    {
+        printf("Cannot open file");
+        exit(0);
+    }
+    c=fgetc(fp);
+    printf("\n");
+    while(c!=EOF)
+    {
+        d=c;
+        while(d==y)
+        {
+            c=fgetc(fp);
+            printf("%c",c);
+            if(c=='\n')
+                goto label;
+        }
+        c=fgetc(fp);
+    }
+    label:
+    {
+        rewind(fp);
+        fclose(fp);
+        printf("\n");
+    }
+}
