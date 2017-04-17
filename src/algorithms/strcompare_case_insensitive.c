a0796b55928bc44ac8edd4f9fedaeaf400e0dc73
Sujata Dev <sujatadev97@gmail.com>
2017-04-18 01:08:35 +0530

    added comments

--- src/algorithms/strcompare_case_insensitive.c
+++ src/algorithms/strcompare_case_insensitive.c
@@ -5,8 +5,10 @@
 int strcompare_case_insensitive(char a[],char b[])
 {
     size_t len1=strlen(a),len2=strlen(b);
+    //lengths should be equal
     if(len1!=len2)
         return 0;
+    //converting strings to lowercase and then comparing
     for(size_t i=0;i<len1;++i)
         if(tolower(a[i])!=tolower(b[i]))
             return 0;
