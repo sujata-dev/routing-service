de1d3bfede1c720dbdccbfec391b8bd80ccf38a9
Sujata Dev <sujatadev97@gmail.com>
2017-04-18 02:56:33 +0530

    removed unnecessary dash

--- src/info_and_initialisation/SIT_landmarks.c
+++ src/info_and_initialisation/SIT_landmarks.c
@@ -5,20 +5,20 @@
 void SIT_landmarks()
 {
     printf("\n\nList of major landmarks of SIT to visit:\n");
-    printf("1) Hostel-A\n");
-    printf("2) Hostel-B\n");
-    printf("3) Hostel-C\n");
-    printf("4) Hostel-D\n");
-    printf("5) Hostel-E\n");
-    printf("6) Hostel-F\n");
-    printf("7) Hostel-G\n");
-    printf("8) Hostel-H\n");
-    printf("9) Hostel-S\n");
+    printf("1) Hostel A\n");
+    printf("2) Hostel B\n");
+    printf("3) Hostel C\n");
+    printf("4) Hostel D\n");
+    printf("5) Hostel E\n");
+    printf("6) Hostel F\n");
+    printf("7) Hostel G\n");
+    printf("8) Hostel H\n");
+    printf("9) Hostel S\n");
     printf("10) CCD (Cafe Coffee Day)\n");
     printf("11) Symbus\n");
     printf("12) Mess\n");
     printf("13) Rangoli\n");
-    printf("14) Coffee-Stop\n");
-    printf("15) Academic-Block\n");
-    printf("16) Main-Gate\n");
+    printf("14) Coffee Stop\n");
+    printf("15) Academic Block\n");
+    printf("16) Main Gate\n");
 }
