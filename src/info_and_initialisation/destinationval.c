a097695043639624fe00a738674f9fc42975a037
Sujata Dev <sujatadev97@gmail.com>
2017-04-18 02:54:33 +0530

    removed unnecessary dash

--- src/info_and_initialisation/destinationval.c
+++ src/info_and_initialisation/destinationval.c
@@ -5,22 +5,22 @@
 int destinationval(char d[])
 {
     int x=0;
-    if(strcompare_case_insensitive(d,"Hostel-A"))   x=1;
-    else if(strcompare_case_insensitive(d,"Hostel-B"))  x=2;
-    else if(strcompare_case_insensitive(d,"Hostel-C"))  x=3;
-    else if(strcompare_case_insensitive(d,"Hostel-D"))  x=4;
-    else if(strcompare_case_insensitive(d,"Hostel-E"))  x=5;
-    else if(strcompare_case_insensitive(d,"Hostel-F"))  x=6;
-    else if(strcompare_case_insensitive(d,"Hostel-G"))  x=7;
-    else if(strcompare_case_insensitive(d,"Hostel-H"))  x=8;
-    else if(strcompare_case_insensitive(d,"Hostel-S"))  x=9;
+    if(strcompare_case_insensitive(d,"Hostel A"))   x=1;
+    else if(strcompare_case_insensitive(d,"Hostel B"))  x=2;
+    else if(strcompare_case_insensitive(d,"Hostel C"))  x=3;
+    else if(strcompare_case_insensitive(d,"Hostel D"))  x=4;
+    else if(strcompare_case_insensitive(d,"Hostel E"))  x=5;
+    else if(strcompare_case_insensitive(d,"Hostel F"))  x=6;
+    else if(strcompare_case_insensitive(d,"Hostel G"))  x=7;
+    else if(strcompare_case_insensitive(d,"Hostel H"))  x=8;
+    else if(strcompare_case_insensitive(d,"Hostel S"))  x=9;
     else if(strcompare_case_insensitive(d,"CCD"))   x=10;
     else if(strcompare_case_insensitive(d,"Symbus"))    x=11;
     else if(strcompare_case_insensitive(d,"Mess"))  x=12;
     else if(strcompare_case_insensitive(d,"Rangoli"))   x=13;
-    else if(strcompare_case_insensitive(d,"Coffee-Stop"))   x=14;
-    else if(strcompare_case_insensitive(d,"Academic-Block"))   x=15;
-    else if(strcompare_case_insensitive(d,"Main-Gate")) x=16;
+    else if(strcompare_case_insensitive(d,"Coffee Stop"))   x=14;
+    else if(strcompare_case_insensitive(d,"Academic Block"))   x=15;
+    else if(strcompare_case_insensitive(d,"Main Gate")) x=16;
     else
         printf("The entered destination node is not available\n");
     return x;
